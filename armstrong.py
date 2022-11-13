2c41d16e373caea7d55272b9eb913f3fb6258d13
srics-dev <mshrikanth315@gmail.com>
2022-11-13 10:11:10 +0530

    init

---
+++ armstrong.py
@@ -0,0 +1,15 @@
+print("THE ARMSTRONG NUMBER CALCULATOR")
+number = int(input("enter the number"))
+length=len(str(number))
+sum=0
+while number>0:
+ digit=number%10
+ sum+=digit*digit*length   
+ number=number//10
+print(sum) 
+
+
+
+
+
+
---
+++ factorial.py
@@ -0,0 +1,10 @@
+print("FACTORIAL OF THE NUMBER")
+print("till where the  number is")
+n=int(input("enter number"))
+i=1
+fac=1
+while i<=n:
+    fac=i*fac
+    i+=1
+    print("factorial of the number")
+    print(fac,"\n")
