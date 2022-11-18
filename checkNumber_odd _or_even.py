423aa39248501beeb13e4eb44049931ef53e85a5
srics-dev <mshrikanth315@gmail.com>
2022-11-18 23:29:07 +0530

    init

---
+++ checkNumber_odd _or_even.py
@@ -0,0 +1,6 @@
+#chekking th number is odd or even
+number= int(input("enter the number"))
+if 0==number%2:
+    print("this number is even")
+else:
+    print("this number is odd")                  
