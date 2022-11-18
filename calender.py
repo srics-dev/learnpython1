d9117fb9a7697f6a46c0921ec55954392427c599
srics-dev <mshrikanth315@gmail.com>
2022-11-18 23:19:30 +0530

    init

---
+++ calender.py
@@ -0,0 +1,6 @@
+#here we are using calender library 
+#by givig the month and year we can get the calender
+import calender
+month=int(input("month mm"))
+year=int(input(('year yy')))
+print(calender.month(year,month))
---
+++ checkPositiveNegetiveZero_number.py
@@ -0,0 +1,8 @@
+#this prograam for to find the posive or negetive or zero
+number=int(input("enter the number\n"))
+if number==0:
+    print("number is zero")
+elif number<0:
+    print("negetive number")
+elif number>0:
+    print("positive number")
