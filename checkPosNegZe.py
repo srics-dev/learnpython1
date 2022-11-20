4abcd8db91b550cefdbd90f69e7ea7046e3bbc41
srics-dev <mshrikanth315@gmail.com>
2022-11-20 21:18:37 +0530

    init

---
+++ checkPosNegZe.py
@@ -0,0 +1,8 @@
+#chekking the number is positivenegetive or zero
+number= int(input("enter the number"))
+if number==0:
+    print("It is a zero")
+elif number>0:
+    print("positive number")
+else:
+    print("it is a negetive number")                  
