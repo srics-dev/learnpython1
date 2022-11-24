d1779f9a1d1efd4c07a8829d8e1c6e1c334892f7
srics-dev <mshrikanth315@gmail.com>
2022-11-24 21:40:30 +0530

    init

---
+++ addIntRange.py
@@ -0,0 +1,9 @@
+print("THIS IS THE PROGRAM TO ADD TILL TO SOME RANGE")
+ange=int(input("enter the range to add the numbers\nfrom till where"))
+i=1
+sum=0
+for i in range(0,ange):
+  sum+=i
+  print(sum)
+print("this is the sum o fthe elements\n",sum)  
+  
---
+++ asciiValueChar.py
@@ -0,0 +1,11 @@
+charecter=input("enter the charecter")
+des=ord(charecter)
+print("the ascii value  of the charecter")
+print(charecter,"\t",des)
+charecter1=input("enter the string")
+for i in charecter1:
+    des1=ord(i)
+    print(i,"\t",des1)
+    
+print("thes eare the value of the respective charecteristic")
+    
